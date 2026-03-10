6e39cae0d0a315f86da033255d3b582333facdca
Kaan Korap <s35063@pjwstk.edu.pl>
2026-03-10 10:45:30 +0100

    Add helper information about the application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from PGO Git exercise.");
+        System.out.println("Application info: PGO Git exercise project");
     }
 }
